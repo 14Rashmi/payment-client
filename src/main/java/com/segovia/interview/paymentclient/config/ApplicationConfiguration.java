7ec5ef8a67e5f40d01eb121e2b93fa0976229ce5
14Rashmi <48930455+14Rashmi@users.noreply.github.com>
2023-06-02 01:34:57 +0100

    Add Swagger Documentation

--- src/main/java/com/segovia/interview/paymentclient/config/ApplicationConfiguration.java
+++ src/main/java/com/segovia/interview/paymentclient/config/ApplicationConfiguration.java
@@ -1,13 +1,18 @@
 package com.segovia.interview.paymentclient.config;
 
-import com.segovia.interview.paymentclient.service.PaymentProcessor;
 import com.segovia.interview.paymentclient.service.SessionTokenManager;
-import com.segovia.interview.paymentclient.util.CsvHelper;
+import com.segovia.interview.paymentclient.service.impl.SessionTokenManagerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class ApplicationConfiguration {
 
     @Bean
@@ -17,7 +22,15 @@ public class ApplicationConfiguration {
 
     @Bean
     public SessionTokenManager sessionTokenManager(RestTemplate restTemplate) {
-        return new SessionTokenManager(restTemplate);
+        return new SessionTokenManagerImpl(restTemplate);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.segovia.interview.paymentclient.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
